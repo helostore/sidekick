12d7f2dd4050cbbe83852e221e81cb7879d1e636
Sergiu Valentin Vlad <sergiu@helostore.com>
2020-04-12 17:01:36 +0800

    Add option to disable ajax settings update

--- app/addons/sidekick/func.php
+++ app/addons/sidekick/func.php
@@ -153,18 +153,18 @@ function fn_sidekick_check($addon)
 	return false;
 }
 
-function fn_sidekick_info($productCode, $showSecurePasswordsButton = false)
+function fn_sidekick_info($productCode, $showSecurePasswordsButton = false, $useAjax = true)
 {
     // Attempt to load new client implementation
     if (class_exists('\HeloStore\ADLS\LicenseClientFactory', true)) {
         $client = \HeloStore\ADLS\LicenseClientFactory::build();
-        return $client->getEnvironment()->helperInfo($productCode, $showSecurePasswordsButton);
+        return $client->getEnvironment()->helperInfo($productCode, $showSecurePasswordsButton, $useAjax);
     }
 
     // Fallback on the old client implementation
     if (class_exists('\HeloStore\ADLS\LicenseClient', true)) {
         if (method_exists('\HeloStore\ADLS\LicenseClient', 'helperInfo')) {
-            return LicenseClient::helperInfo($productCode, $showSecurePasswordsButton);
+            return LicenseClient::helperInfo($productCode, $showSecurePasswordsButton, $useAjax);
         }
 	}
 
